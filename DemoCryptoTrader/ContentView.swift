86668336f542ffb187dbd1b03f3c3184c83b6513
Anand Jee Choubey <anandjechoubey@gmail.com>
2023-12-19 14:34:33 +0530

    Initial Commit

---
+++ DemoCryptoTrader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoCryptoTrader
+//
+//  Created by Anand Jee Choubey on 2023-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoCryptoTrader/DemoCryptoTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCryptoTraderApp.swift
+//  DemoCryptoTrader
+//
+//  Created by Anand Jee Choubey on 2023-12-19.
+//
+
+import SwiftUI
+
+@main
+struct DemoCryptoTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
